fec4956f03c05ca5504172fe04c25cb6a88461e4
RedWarlock66 <125387221+RedWarlock66@users.noreply.github.com>
2023-06-01 21:45:30 +0400

    Add files via upload
    automatically opening UI in the default browser and requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request, make_response, render_template, jsonify
 from GPT import ConversationAPI
+from threading import Thread
+from pathlib import Path
+from json import load
+import webbrowser, requests, time
 
-srvr = "http://127.0.0.1:5000"
+app_settings_file, srvr = Path(__file__).resolve().parent / 'settings/app_settings.json', f"http://127.0.0.1"
 app, gpt_api = Flask(__name__), None
 #пока что один объект на всех пользователей приложения с одним общим диалогом на всех
 #потом нужно будет или хранить диалог в разрезе пользователей (нагружает сервер), или хранить его на клиенте
 # (нагружает канал связи). решим по месту
 gpt_api = ConversationAPI()
+
+@app.route('/gpt/ping', methods=['GET'])
+def ping():
+    return 'Enhanced GPT conversation is working'
 
 @app.route('/gpt', methods=['GET'])
 @app.route('/gpt/index', methods=['GET'])
@@ -37,5 +45,21 @@ def set_openai_key():
     gpt_api.openai_key = request.get_data(as_text=True)
     return make_response('Ok', 200)
 
+def _read_settings() -> dict:
+    with open(app_settings_file, 'r') as file:
+       app_settings = load(file)
+    return app_settings
+
+def _open_ui(port, **kwargs):
+    while True:
+        time.sleep(1)
+        result = requests.get(f'{srvr}:{port}/gpt/ping')
+        if result.status_code == 200:
+            webbrowser.open_new_tab(f'{srvr}:{port}/gpt')
+            break
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app_settings = _read_settings()
+    ui_thread = Thread(target=_open_ui, kwargs=app_settings)
+    ui_thread.start()
+    app.run(**app_settings)
